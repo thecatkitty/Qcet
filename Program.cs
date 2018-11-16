0a2c24391a9d89d12cd83c2e64c7778a9063c31e
Mateusz Karcz <mateusz.karcz@interia.eu>
2018-11-16 23:18:00 +0100

    Create a minimal API with one endpoint.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Qcet {
+  /// <summary>Program entry class.</summary>
+  public class Program {
+    /// <summary>Program entry method.</summary>
+    public static void Main(string[] args) {
+      CreateWebHostBuilder(args).Build().Run();
+    }
+
+    /// <summary>Create web host builder using Startup class.</summary>
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+      WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+  }
+}
