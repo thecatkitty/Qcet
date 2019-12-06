40e93035a474408385fbe903330228945b86b270
Mateusz Karcz <mateusz.karcz@interia.eu>
2019-12-06 01:48:23 +0100

    Show device info

---
+++ Qcet/Platform.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Qcet
+{
+    public static class Platform
+    {
+        public static string Version {
+            get {
+                if (Device.RuntimePlatform == Device.WPF)
+                {
+                    return string.Format(
+                        "WPF {0}",
+                        Environment.Version);
+                }
+                
+                return string.Format(
+                    "{0} {1}",
+                    DeviceInfo.Platform,
+                    DeviceInfo.VersionString);
+            }
+        }
+
+        public static string Manufacturer {
+            get {
+                if (Device.RuntimePlatform == Device.WPF)
+                {
+                    return DeviceIdiom.Unknown.ToString();
+                }
+
+                return DeviceInfo.Manufacturer;
+            }
+        }
+
+        public static string Model {
+            get {
+                if (Device.RuntimePlatform == Device.WPF)
+                {
+                    return DeviceIdiom.Unknown.ToString();
+                }
+
+                return DeviceInfo.Model;
+            }
+        }
+    }
+}
