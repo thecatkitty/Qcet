ae3790b0c38baae92c2d336fc53aa0c082cd9bad
Mateusz Karcz <mateusz.karcz@interia.eu>
2019-12-06 01:52:28 +0100

    Build for WPF (Windows 7-10)

---
+++ Qcet.WPF/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF;
+
+namespace Qcet.WPF
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+            LoadApplication(new Qcet.App());
+        }
+
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+            var topbar = this.Template.FindName("PART_TopAppBar", this) as Xamarin.Forms.Platform.WPF.Controls.FormsAppBar;
+            if (topbar != null) topbar.MaxHeight = 0;
+        }
+    }
+}
