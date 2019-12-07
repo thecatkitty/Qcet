5017b7f94d8725267db8456b0a865c8cae56cd4c
Mateusz Karcz <35971468+thecatkitty@users.noreply.github.com>
2019-12-07 21:49:23 +0100

    PS API Client: Add PowerShell API client

---
+++ FundacjaBT.EventTool/ApiClient.ps1
@@ -0,0 +1,9 @@
+﻿[reflection.assembly]::LoadFile($PSScriptRoot + "/bin/Debug/netstandard2.0/FundacjaBT.EventTool.dll")
+
+$Api = New-Object FundacjaBT.EventTool.ApiClient
+$Api.Address = [uri]"http://localhost:8000/"
+
+$Cred = (Get-Credential).GetNetworkCredential()
+$Cred.Domain = $Api.Address.DnsSafeHost
+
+$Api.Connect($Cred)
